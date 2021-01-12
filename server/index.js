8dc2858aa5e7a8d7b921a7187b34553da94ad65d
Benjamin <benjaminbotros@outlook.com>
2021-01-12 15:59:50 +0100

    Working Connnection to database

--- server/index.js
+++ server/index.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const app = express();
+const mysql = require('mysql');
+
+const db = mysql.createPool({
+   host: "localhost",
+   user: "root",
+   password: "password",
+   database: "portfolio2021"
+});
+
+
 
 app.get('/', (req, res) => {
-   res.send('hello friend');
+   const sqlInsert = "INSERT INTO `Test` (`Name`) VALUES ('whatevs');"   
+   db.query(sqlInsert, (err, result) => {
+      if (err) throw err;
+      console.log("Connected!");
+   });
 })
 
-app.listen(3002, () => {
-   console.log('running on port 3002');
+app.listen(3006, () => {
+   console.log('running on port 3006');
 })
+
